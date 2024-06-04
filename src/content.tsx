85eb92fb95b78194323584d5c63e6c99ac4201dc
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-06-04 14:00:37 +0200

    Refactor content rendering to use React and ReactDOM

--- src/content.tsx
+++ src/content.tsx
@@ -1,3 +1,23 @@
-const el = document.createElement('p');
-el.innerText = 'Hello, world!';
-document.body.appendChild(el);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function injectExtensionElement() {
+  const extensionElement = document.createElement("rawr-extension");
+  const shadowRoot = extensionElement.attachShadow({ mode: "open" });
+  const root = document.createElement("div");
+
+  root.id = "root";
+  shadowRoot.appendChild(root);
+
+  document.documentElement.appendChild(extensionElement);
+
+  return root;
+}
+
+const root = injectExtensionElement();
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <h1>Hello World!</h1>
+  </React.StrictMode>
+);
