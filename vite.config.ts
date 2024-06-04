c99b60a57adcb0672303e7100175c1cf8fbddc52
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-06-04 08:32:41 +0200

    Update Vite build configuration for rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        background: 'src/background.ts',
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      }
+    }
+  }
 })
